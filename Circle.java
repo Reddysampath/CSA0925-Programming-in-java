bb335ee2bfc4b5a9549cdaf1fce8fc6f12ad3481
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-02-13 22:29:09 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Circle
+{
+    public static void main(String arg[])
+     {
+       float a;
+       int r;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the length:");
+       r=sc.nextInt();
+       a=3.14*r*r;
+       System.out.println("Area of circle="+a);
+     }
+}
+       
