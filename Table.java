04da8af696ca1758f7ee3efb5b3bfbbe46ba0cbc
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-02-18 12:33:05 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Table
+{
+  public static void main(String arg[])
+   {
+      Scanner sc=new Scanner(System.in);
+      int M=4;
+      int N=5;
+      for(int i=1;i<=N;i++)
+      {
+        System.out.println(i+"x"+M+"="+(i*M));
+      }
+  }
+}
