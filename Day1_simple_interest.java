b5018b41005c30335ad732e412795d074b81b69a
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-03-22 13:41:57 +0530

    Add files via upload

---
+++ Day1_simple_interest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Day1_simple_interest
+{
+   public static void main(String arg[])
+    {
+       int P,N,R,SI;
+       P=10000;
+       R=2;
+       N=12;
+       SI=(P*N*R)/100;
+       System.out.println("The simple interest is:"+SI);
+     }
+}
