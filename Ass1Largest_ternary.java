a3c56ecb5a71c46826dea4ef583f4fc67ea3b37c
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-03-22 13:55:48 +0530

    Add files via upload

---
+++ Ass1Largest_ternary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Ass1Largest_ternary
+{
+public static void main(String arg[])
+{
+int a=23,b=12,c=15;
+int largest=((a>b)?(a>c)? a:c:(b>c)? b:c);
+System.out.println(" Largest is"+largest);
+}
+}
