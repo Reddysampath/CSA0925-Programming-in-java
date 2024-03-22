f734bbafb643c70b3bbccc73e3d5322ef1d18ee5
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-03-22 13:37:14 +0530

    Add files via upload

---
+++ Day1.1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Day1.1
+{
+   public static void main(String arg[])
+    {
+       int a,b,c;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the a value:");
+       a=sc.nextInt();
+       System.out.println("Enter the b value:");
+       b=sc.nextInt();
+       c=a-b;
+       System.out.println("Sub of two numbers is:" +c);
+     }
+}
