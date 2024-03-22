5665d96a57fda3d9261f6ab6e7c98302c6122b7a
Reddysampath <124856789+Reddysampath@users.noreply.github.com>
2024-03-22 13:37:51 +0530

    Add files via upload

---
+++ Day1_AND_OR_NOT.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Day1_AND_OR_NOT
+{
+public static void main(String arg[])
+{
+int a=15,b=8;
+if(a>12 && b<10)
+{
+System.out.println("true"); 
+}
+else
+{
+System.out.println("false"); 
+}
+if(a<10 || b<10)
+{
+System.out.println("true"); 
+}
+else
+{
+System.out.println("false"); 
+}
+if(a!=b)
+{
+System.out.println("false"); 
+}
+else
+{
+System.out.println("true");
+}
+}
+}
